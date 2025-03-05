17a9841bc1ad668d7e13690bf9e19c5337dd43ac
jomarcello <104986494+jomarcello@users.noreply.github.com>
2025-03-05 16:22:03 +0700

    Update __init__.py

---
+++ trading_bot/__init__.py
@@ -0,0 +1,9 @@
+# Import hack voor backward compatibility
+from trading_bot.services.chart_service.tradingview_selenium import TradingViewSeleniumService
+from trading_bot.services.chart_service.tradingview_playwright import TradingViewPlaywrightService
+
+# Voor backward compatibility
+TradingViewPuppeteerService = TradingViewPlaywrightService
+
+
+
