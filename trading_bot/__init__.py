95d65550d01e3e56aa5653cd7592ffbf64097b3c
jomarcello <104986494+jomarcello@users.noreply.github.com>
2025-03-20 13:03:20 +0700

    Update __init__.py

--- trading_bot/__init__.py
+++ trading_bot/__init__.py
@@ -1,9 +1,12 @@
 # Import hack voor backward compatibility
 from trading_bot.services.chart_service.tradingview_selenium import TradingViewSeleniumService
 from trading_bot.services.chart_service.tradingview_playwright import TradingViewPlaywrightService
+from trading_bot.services.telegram_service.bot import TelegramService
 
 # Voor backward compatibility
 TradingViewPuppeteerService = TradingViewPlaywrightService
 
 # Leeg bestand of minimale imports
 # Vermijd het importeren van ChartService en TradingViewSeleniumService hier
+
+__all__ = ['TelegramService', 'TradingViewSeleniumService']
