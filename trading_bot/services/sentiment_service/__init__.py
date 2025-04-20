9e4cb3a3acf54255a9c2f0ddc80cddf46d4eb125
jomarcello <104986494+jomarcello@users.noreply.github.com>
2025-04-20 19:23:53 +0200

    Update __init__.py

--- trading_bot/services/sentiment_service/__init__.py
+++ trading_bot/services/sentiment_service/__init__.py
@@ -1,5 +1,6 @@
-#
+# Sentiment service initialization
 
+# Import the MarketSentimentService directly to make it available
 from trading_bot.services.sentiment_service.sentiment import MarketSentimentService
 
 __all__ = ["MarketSentimentService"]
