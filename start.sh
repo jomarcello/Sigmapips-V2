4d70498e1ff8ed881edb1b5289645e2924690997
jomarcello <104986494+jomarcello@users.noreply.github.com>
2025-04-22 08:00:17 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -6,7 +6,7 @@ export SUPABASE_URL="https://utigkgjcyqnrhpndzqhs.supabase.co"
 export SUPABASE_KEY="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InV0aWdrZ2pjeXFucmhwbmR6cWhzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzYzMjMwNTYsImV4cCI6MjA1MTg5OTA1Nn0.QyCD9UnG80F8bTIK4dOMQagEo3iDgPiFm4azldv58Xo"
 export DEEPSEEK_API_KEY="sk-274ea5952e7e4b87aba4b14de3990c7d"
 export USE_MOCK_DATA="false"
-export FORCE_POLLING="false"
+export FORCE_POLLING="true"
 
-# Run the bot
-python3 main.py
+# Run the bot directly with the module path
+python -m trading_bot.main
